a41e5eaa16cee6369ab981dc7662b36ccea49727
Rafael <portugal93>
2016-11-30 11:49:38 +0000

    can now create fixed tours

--- src/main/webapp/app/tour/tour-new.controller.js
+++ src/main/webapp/app/tour/tour-new.controller.js
@@ -1,62 +1,74 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('tourologistApp')
         .controller('CreateTourController', TourDialogController);
 
-    TourDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Tour', 'User', 'Interest', 'TourRating', 'TourDownload', 'TourImage', 'TourAdminReview', 'TourRoutePoint', 'TourBubbl'];
+    TourDialogController.$inject = ['$timeout', '$scope', 'Bubbl', '$uibModalInstance', 'entity', 'Tour', 'User', 'Interest'];
 
-    function TourDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, Tour, User, Interest, TourRating, TourDownload, TourImage, TourAdminReview, TourRoutePoint, TourBubbl) {
+    function TourDialogController($timeout, $scope, Bubbl, $uibModalInstance, entity, Tour, User, Interest) {
         var vm = this;
 
         vm.tour = entity;
         vm.clear = clear;
-        vm.datePickerOpenStatus = {};
-        vm.openCalendar = openCalendar;
+
         vm.save = save;
         vm.users = User.query();
         vm.interests = Interest.query();
-        vm.tourratings = TourRating.query();
-        vm.tourdownloads = TourDownload.query();
-        vm.tourimages = TourImage.query();
-        vm.touradminreviews = TourAdminReview.query();
-        vm.tourroutepoints = TourRoutePoint.query();
-        vm.tourbubbls = TourBubbl.query();
 
-        $timeout(function (){
+        vm.bubbls = Bubbl.query();
+        vm.add = add;
+        vm.removebubbl = removebubbl;
+
+
+        if (vm.tour.bubbls === undefined) {
+            vm.tour.bubbls = [];
+        }
+
+        if (vm.tour.bubbls.length === 0) {
+            vm.tour.bubbls.push({
+                bubblId: '',
+                orderNumber: ''
+            });
+        }
+
+
+        function removebubbl(bubblIndex) {
+            console.log('removing ingredient with id ' + bubblIndex);
+            vm.tour.bubbls.splice(bubblIndex, 1);
+        }
+
+        function add() {
+            vm.tour.bubbls.push({
+                bubblId: '',
+                orderNumber: ''
+            });
+        }
+
+        $timeout(function () {
             angular.element('.form-group:eq(1)>input').focus();
         });
 
-        function clear () {
+        function clear() {
             $uibModalInstance.dismiss('cancel');
         }
 
-        function save () {
+        function save() {
             vm.isSaving = true;
-            if (vm.tour.id !== null) {
-                Tour.update(vm.tour, onSaveSuccess, onSaveError);
-            } else {
-                Tour.save(vm.tour, onSaveSuccess, onSaveError);
-            }
+            Tour.createFixedTour(vm.tour, onSaveSuccess, onSaveError);
         }
 
-        function onSaveSuccess (result) {
+        function onSaveSuccess(result) {
             $scope.$emit('tourologistApp:tourUpdate', result);
             $uibModalInstance.close(result);
             vm.isSaving = false;
         }
 
-        function onSaveError () {
+        function onSaveError() {
             vm.isSaving = false;
         }
 
-        vm.datePickerOpenStatus.createdDate = false;
-        vm.datePickerOpenStatus.lastModified = false;
-        vm.datePickerOpenStatus.deleted = false;
 
-        function openCalendar (date) {
-            vm.datePickerOpenStatus[date] = true;
-        }
     }
 })();
