6b74f945cfdc8ba5b350f9d7c7e4605ddc8b9db5
Rafael Magalhaes <rafaelmagalhaes93@gmail.com>
2016-12-02 14:47:11 +0000

    can now approve a bubbl or reject a bubbl, edit payload button removed

--- src/main/webapp/app/bubbl/bubbl-detail.controller.js
+++ src/main/webapp/app/bubbl/bubbl-detail.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -13,9 +13,65 @@
         vm.bubbl = entity;
         vm.previousState = previousState.name;
 
-        var unsubscribe = $rootScope.$on('tourologistApp:bubblUpdate', function(event, result) {
+        var unsubscribe = $rootScope.$on('tourologistApp:bubblUpdate', function (event, result) {
             vm.bubbl = result;
         });
+
+        $scope.approve = function () {
+
+            var approve = {
+
+                description: vm.bubbl.description,
+                id: vm.bubbl.id,
+                lat: vm.bubbl.lat,
+                lng: vm.bubbl.lng,
+                name: vm.bubbl.name,
+                radiusMeters: vm.bubbl.radiusMeters,
+                status: "APPROVED",
+                interests:vm.bubbl.interests,
+                userEmail: vm.bubbl.userEmail,
+                userId: vm.bubbl.userId
+
+            };
+            if (vm.bubbl.id !== null) {
+                Bubbl.update(approve, onSaveSuccess, onSaveError);
+            }
+        };
+
+        $scope.rejectBubbl = function () {
+            var approve = {
+
+                description: vm.bubbl.description,
+                id: vm.bubbl.id,
+                lat: vm.bubbl.lat,
+                lng: vm.bubbl.lng,
+                name: vm.bubbl.name,
+                radiusMeters: vm.bubbl.radiusMeters,
+                status: "REJECTED",
+                interests:vm.bubbl.interests,
+                userEmail: vm.bubbl.userEmail,
+                userId: vm.bubbl.userId
+
+            };
+            if (vm.bubbl.id !== null) {
+                Bubbl.update(approve, onSaveSuccess, onSaveError);
+            }
+
+        };
+
+        function onSaveSuccess(result) {
+            vm.isSaving = true;
+
+            $scope.$emit('tourologistApp:bubblUpdate', result);
+
+
+        }
+
+        function onSaveError() {
+            vm.isSaving = false;
+
+        }
+
         $scope.$on('$destroy', unsubscribe);
     }
 })();
