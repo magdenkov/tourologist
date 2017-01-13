a1911275e91abe8ea821774e0365b3fa840b2a14
anna.miroshnik <miroshnik.anna@gmail.com>
2017-01-13 14:01:20 +0300

    aside (first version)

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -15,6 +15,7 @@
             'ui.bootstrap.datetimepicker',
             'ui.router',
             'infinite-scroll',
+            'ngAnimate',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
             'angular-loading-bar',
             'uiGmapgoogle-maps',
